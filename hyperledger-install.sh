0644e10cd05046021f50e61e8eb2d7a24b323cd6
kyriosdata <fabio@inf.ufg.br>
2019-12-17 15:31:36 +0000

    remove download go

--- hyperledger-install.sh
+++ hyperledger-install.sh
@@ -29,6 +29,8 @@ curl -O https://storage.googleapis.com/golang/go1.12.9.linux-amd64.tar.gz
 tar -xvf go1.12.9.linux-amd64.tar.gz
 sudo chown -R root:root ./go
 sudo mv go /usr/local
+rm -f go1.12.9.linux-amd64.tar.gz
+
 mkdir $HOME/go
 echo export GOPATH=$HOME/go >> $HOME/.bashrc
 echo export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin:$HOME/fabric-samples/bin >> $HOME/.bashrc
