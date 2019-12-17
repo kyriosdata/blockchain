9b2f2f48af16bdb7a67627b229c56e7dc357f510
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-12-17 12:08:31 -0300

    script atualização

--- hyperledger-install.sh
+++ hyperledger-install.sh
@@ -1,11 +1,24 @@
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-#. ~/.nvm/nvm.sh
-#nvm install node
+# docker
+
+sudo apt-get update
+sudo apt-get -y remove docker docker-engine docker.io
+sudo apt -y install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# docker-compose
+
+sudo apt -y install docker-compose
+
+# node (e npm)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
 
 # JAVA E MAVEN
 
-#sudo apt -y install openjdk-8-jdk-headless
-#sudo apt -y install maven
+sudo apt -y install openjdk-8-jdk-headless
+sudo apt -y install maven
 
 # GO lang
 curl -O https://storage.googleapis.com/golang/go1.12.9.linux-amd64.tar.gz
