cc6de9bd86b92a7f4f423d52e78e01cb2955d1c0
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2019-12-20 21:29:29 -0300

    usuario a ser registrado generalizado em constante

--- samples/fabcar/src/main/java/org/example/RegisterUser.java
+++ samples/fabcar/src/main/java/org/example/RegisterUser.java
@@ -20,6 +20,9 @@ import org.hyperledger.fabric_ca.sdk.RegistrationRequest;
 
 public class RegisterUser {
 
+	private static final String URL = "https://3.231.207.0:7054";
+	private static final String USER = "davi";
+
 	static {
 		System.setProperty("org.hyperledger.fabric.sdk.service_discovery.as_localhost", "true");
 	}
@@ -30,7 +33,7 @@ public class RegisterUser {
 		Properties props = new Properties();
 		props.put("pemFile","./ca.org1.example.com-cert.pem");
 		props.put("allowAllHostNames", "true");
-		HFCAClient caClient = HFCAClient.createNewInstance("https://3.231.207.0:7054", props);
+		HFCAClient caClient = HFCAClient.createNewInstance(URL, props);
 		CryptoSuite cryptoSuite = CryptoSuiteFactory.getDefault().getCryptoSuite();
 		caClient.setCryptoSuite(cryptoSuite);
 
@@ -38,9 +41,10 @@ public class RegisterUser {
 		Wallet wallet = Wallet.createFileSystemWallet(Paths.get("wallet"));
 
 		// Check to see if we've already enrolled the user.
-		boolean userExists = wallet.exists("fabio");
+		boolean userExists = wallet.exists(USER);
 		if (userExists) {
-			System.out.println("An identity for the user \"user1\" already exists in the wallet");
+			System.out.printf("An identity for the user \"%s\" already " +
+					"exists in the wallet", USER);
 			return;
 		}
 
@@ -98,14 +102,15 @@ public class RegisterUser {
 
 		// Register the user, enroll the user, and import the new identity into the wallet.
 		RegistrationRequest registrationRequest = new RegistrationRequest(
-				"fabio");
+				USER);
 		registrationRequest.setAffiliation("org1.department1");
-		registrationRequest.setEnrollmentID("fabio");
+		registrationRequest.setEnrollmentID(USER);
 		String enrollmentSecret = caClient.register(registrationRequest, admin);
-		Enrollment enrollment = caClient.enroll("fabio", enrollmentSecret);
+		Enrollment enrollment = caClient.enroll(USER, enrollmentSecret);
 		Identity user = Identity.createIdentity("Org1MSP", enrollment.getCert(), enrollment.getKey());
-		wallet.put("fabio", user);
-		System.out.println("Successfully enrolled user \"user1\" and imported it into the wallet");
+		wallet.put(USER, user);
+		System.out.printf("Successfully enrolled user \"%s\" and " +
+				"imported it into the wallet", USER);
 	}
 
 }
